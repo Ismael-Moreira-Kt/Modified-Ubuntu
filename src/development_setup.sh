bff505dba28cd1ee697be3b2937970fd3c013e76
Ismael-Moreira-Kt <ismaelmoreiraubuntu@gmail.com>
2024-07-22 07:49:44 +0100

    feat: I added the basic tools installation.

--- src/development_setup.sh
+++ src/development_setup.sh
@@ -4,6 +4,19 @@
 
 
 
+function preparing_installation() {
+    echo -e "\n\n"
+    echo "Updating the package list..."
+    sudo apt update -y && sudo apt upgrade -y 
+    sudo apt autoremove -y && sudo apt autoclean -y
+    
+    echo -e "\n\n"
+    echo "Installing basic tools..."
+    sudo apt install -y git curl gnupg build-essential
+}
+
+
+
 function development_setup() {
     echo -e "\n\n"
     echo "Preparing for installation..."
