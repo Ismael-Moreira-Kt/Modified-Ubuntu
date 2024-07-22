99bf1bfab58a08f51f23cd5199ed3682b2db3fe8
Ismael-Moreira-Kt <ismaelmoreiraubuntu@gmail.com>
2024-07-22 07:47:55 +0100

    feat: I've added the function that structures the installation of the development setup.

---
+++ src/development_setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+
+
+
+function development_setup() {
+    echo -e "\n\n"
+    echo "Preparing for installation..."
+    preparing_installation
+    
+    clear
+    
+    echo "Installing..."
+    installation
+}
