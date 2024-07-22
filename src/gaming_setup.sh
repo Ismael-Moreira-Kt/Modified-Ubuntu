c5636d7e25c28c5235b6b73349b600182e157241
Ismael-Moreira-Kt <ismaelmoreiraubuntu@gmail.com>
2024-07-22 08:01:43 +0100

    feat: I've added the gaming setup.

---
+++ src/gaming_setup.sh
@@ -0,0 +1,10 @@
+function gaming_setup() {
+    echo -e "\n\n"
+    echo "Preparing for installation..."
+    preparing_installation
+    
+    clear
+    
+    echo "Installing..."
+    installation
+}
