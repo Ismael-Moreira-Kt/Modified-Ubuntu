577c968edd8af5a5b9ee26e9a65f4426e4f929f6
Ismael-Moreira-Kt <ismaelmoreiraubuntu@gmail.com>
2024-07-22 07:53:12 +0100

    feat: I have added the configuration of several services.

--- src/development_setup.sh
+++ src/development_setup.sh
@@ -29,6 +29,26 @@ function preparing_installation() {
     echo "Adding a Docker repository..."
     echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
     sudo apt-get update
+
+        
+    echo -e "\n\n"
+    echo "Adding repositories for Vagrant, HashiCorp, Snap, MongoDB, Jenkins..."
+    wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+    echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+    
+    echo -e "\n\n"
+    sudo apt update
+    sudo apt install snapd
+    
+    echo -e "\n\n"
+    echo "Configuring MongoDB..."
+    curl -fsSL https://www.mongodb.org/static/pgp/server-7.0.asc | sudo gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg --dearmor
+    echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
+    
+    echo -e "\n\n"
+    echo "Configuring Jenkins..."
+    sudo wget -O /usr/share/keyrings/jenkins-keyring.asc https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+    echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/" | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
 }
 
 
