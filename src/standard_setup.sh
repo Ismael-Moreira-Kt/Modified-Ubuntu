ddc8bd2c1d6f0ac4d87cb54bd5450a9f898014d7
Ismael-Moreira-Kt <ismaelmoreiraubuntu@gmail.com>
2024-07-23 06:04:03 +0100

    feat: Creation of the standard method.

---
+++ src/standard_setup.sh
@@ -0,0 +1,18 @@
+function preparing_installation() { }
+
+
+
+function installation() { }
+
+
+
+function standard_setup() {
+    echo -e "\n\n"
+    echo "Preparing for installation..."
+    preparing_installation
+    
+    clear
+    
+    echo "Installing..."
+    installation
+}
