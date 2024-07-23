8908ccdb94c78f0db7ababedeab4ad1266ffd16d
Ismael-Moreira-Kt <ismaelmoreiraubuntu@gmail.com>
2024-07-23 04:43:36 +0100

    feat: I've added the package update.

--- src/gaming_setup.sh
+++ src/gaming_setup.sh
@@ -1,10 +1,28 @@
+function preparing_installation() {
+    echo -e "\n\n"
+    echo "Updating the package list..."
+    sudo apt update -y && sudo apt upgrade -y 
+    sudo apt autoremove -y && sudo apt autoclean -y
+}
+
+
+
+function installation() {
+    echo -e "\n\n"
+    echo "Updating the package list..."
+    sudo apt update -y && sudo apt upgrade -y 
+    sudo apt autoremove -y && sudo apt autoclean -y
+}
+
+
+
 function gaming_setup() {
     echo -e "\n\n"
     echo "Preparing for installation..."
     preparing_installation
-    
+
     clear
-    
+
     echo "Installing..."
     installation
 }
