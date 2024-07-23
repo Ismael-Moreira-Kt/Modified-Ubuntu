cbae1f50def10031a0c995dff447f61fb49a3299
Ismael-Moreira-Kt <ismaelmoreiraubuntu@gmail.com>
2024-07-23 05:07:55 +0100

    feat: I installed and configured Pipewire.

--- src/gaming_setup.sh
+++ src/gaming_setup.sh
@@ -12,6 +12,7 @@ function preparing_installation() {
     echo -e "\n\n"
     echo "Adding repositories..."
     sudo add-apt-repository ppa:atareao/atareao
+    sudo add-apt-repository ppa:pipewire-debian/pipewire-upstream
 
     echo -e "\n\n"
     echo "Installing snap..."
@@ -26,29 +27,47 @@ function installation() {
     sudo apt update -y && sudo apt upgrade -y 
     sudo apt autoremove -y && sudo apt autoclean -y
 
+    echo -e "\n\n"
     echo "Installing CPU-G..."
     sudo apt install -y cpu-g
 
+    echo -e "\n\n"
     echo "Installing flatpak..."
     sudo apt install -y flatpak
     flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
+    echo -e "\n\n"
     echo "Installing Steam..."
     flatpak install flathub com.valvesoftware.Steam
 
+    echo -e "\n\n"
     echo "Installing Lutris..."
     flatpak install flathub net.lutris.Lutris
 
+    echo -e "\n\n"
     echo "Installing Wine..."
     flatpak install flathub org.winehq.Wine
 
+    echo -e "\n\n"
     echo "Installing WineTricks..."
     wget  https://raw.githubusercontent.com/Winetricks/winetricks/master/src/winetricks
     chmod +x winetricks 
     sudo mv -v winetricks /usr/local/bin
 
+    echo -e "\n\n"
     echo "Installing Bottles..."
     flatpak install flathub com.usebottles.bottles
+
+    echo -e "\n\n"
+    echo "Installing and configuring PipeWire..."
+    sudo apt install -y pipewire pipewire-pulse
+    sudo apt install -y pipewire-audio-client-libraries
+    sudo apt install -y gstreamer1.0-pipewire libspa-0.2-bluetooth libspa-0.2-jack
+    systemctl --user stop pulseaudio.service pulseaudio.socket
+    systemctl --user disable pulseaudio.service pulseaudio.socket
+    systemctl --user mask pulseaudio.service pulseaudio.socket
+    systemctl --user enable pipewire.socket pipewire-pulse.socket
+    systemctl --user start pipewire.socket pipewire-pulse.socket
 }
 
 
